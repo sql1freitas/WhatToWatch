22dddc08fa7bfea057924524c865ba36d53b255a
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-06-04 00:20:34 -0300

    Frontend (Etapa 1)

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.staticfiles import StaticFiles
 
 load_dotenv()
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 app = FastAPI()
+
+frontend_path = os.path.join(os.path.dirname(__file__), '../frontend')
+app.mount("/", StaticFiles(directory=frontend_path, html=True), name="static")
